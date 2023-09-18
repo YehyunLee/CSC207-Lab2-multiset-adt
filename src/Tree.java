39d0aaf5e8a05a3894e8acf39cabdc55ab26f134
YehyunLee <glad7cu@gmail.com>
2023-09-18 16:45:17 -0400

    Class Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,15 @@
+//import Any type
+import java.lang.Object;
+
 public class Tree {
+    private static Object _root = null;
+    private static Tree[] _subtrees = null;
+    public Tree(Object root, Tree[] subtrees) {
+        _root = root;
+        if (subtrees == null) {
+            _subtrees = new Tree[0];
+        } else {
+            _subtrees = subtrees;
+        }
+    }
 }
